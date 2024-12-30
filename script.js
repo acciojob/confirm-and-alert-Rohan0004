1e48285c860629bbc1bbf4fd9df1e3046e192005
Rohan0004 <110601451+Rohan0004@users.noreply.github.com>
2024-12-30 23:55:08 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,7 @@
 //your JS code here. If required.
+var res = confirm("Do you want to proceed?")
+if (res) {
+	alert("You clicked OK. Proceeding...");
+} else {
+	alert("You clicked Cancel. Exiting...");
+}
